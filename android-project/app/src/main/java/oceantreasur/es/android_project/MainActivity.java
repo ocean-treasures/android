4fb7cb2edbebe7d57a125034ef188db99ee0ec4d
Zachary G <zax333@abv.bg>
2017-07-03 16:17:44 +0300

    Created play button

--- android-project/app/src/main/java/oceantreasur/es/android_project/MainActivity.java
+++ android-project/app/src/main/java/oceantreasur/es/android_project/MainActivity.java
@@ -1,16 +1,29 @@
 package oceantreasur.es.android_project;
 
-import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Typeface;
 import android.os.Bundle;
-import android.view.Window;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
+    private Typeface playButtonTypeFace;
+    private Button playButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+
+        this.playButton = (Button) findViewById(R.id.test1);
+        this.playButtonTypeFace = Typeface.createFromAsset(getAssets(), "fonts/CoolCrayon.ttf");
+
+        playButton.setTypeface(playButtonTypeFace);
+
+        playButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            }
+        });
+
     }
 }
