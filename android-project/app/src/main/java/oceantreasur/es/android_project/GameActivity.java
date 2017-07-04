8d227b44414dd43e4ab7b519c4276b8443b08bfe
Zachary G <zax333@abv.bg>
2017-07-04 17:37:46 +0300

    Added onClickListener to progress bar

--- android-project/app/src/main/java/oceantreasur/es/android_project/GameActivity.java
+++ android-project/app/src/main/java/oceantreasur/es/android_project/GameActivity.java
@@ -1,13 +1,30 @@
 package oceantreasur.es.android_project;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
 
 public class GameActivity extends AppCompatActivity {
+
+    private static final int PROGRESS_MAX = 100;
+
+    private ProgressBar progressBar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        this.progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        this.progressBar.setMax(PROGRESS_MAX);
+
+        progressBar.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                progressBar.setProgress((progressBar.getProgress() + PROGRESS_MAX/10) % PROGRESS_MAX);
+            }
+        });
     }
 }
