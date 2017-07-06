7d4d9cb0127dfc0d02ebcb28a81701d96704bdd6
Stoyan-Ivanov <stoyaniviv@gmail.com>
2017-07-06 11:35:10 +0300

    added wrong screen

---
+++ android-project/app/src/main/java/oceantreasur/es/android_project/WrongAnswerActivity.java
@@ -0,0 +1,13 @@
+package oceantreasur.es.android_project;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WrongAnswerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wrong_answer);
+    }
+}
