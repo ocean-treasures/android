e2b3e029fac86a00331efb1f907b7493a29cecc4
Zachary G <zax333@abv.bg>
2017-07-07 15:35:47 +0300

    // TODO - Debug

--- android-project/app/src/main/java/oceantreasur/es/android_project/OceanTreasuresApplication.java
+++ android-project/app/src/main/java/oceantreasur/es/android_project/OceanTreasuresApplication.java
@@ -3,6 +3,13 @@ package oceantreasur.es.android_project;
 import android.app.Application;
 import android.content.Context;
 
+import java.io.IOException;
+
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.mock.MockRetrofit;
@@ -20,13 +27,35 @@ public class OceanTreasuresApplication extends Application {
         super.onCreate();
         instance = this;
         applicationContext = getApplicationContext();
+
+
+
         createRetrofitInstance();
         FontManager.init(getAssets());
     }
 
     private void createRetrofitInstance() {
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+
+        OkHttpClient httpClient = new OkHttpClient.Builder()
+                .addNetworkInterceptor(interceptor)
+//                .addNetworkInterceptor(new Interceptor() {
+//            @Override
+//            public Response intercept(Chain chain) throws IOException {
+//                Request request = chain.request();
+//
+//                request = request.newBuilder()
+//                        .header("Content-Type", "application/json")
+//                        .build();
+//
+//                return chain.proceed(request);
+//            }
+//        })
+            .build();
+
         Retrofit.Builder builder = new Retrofit.Builder()
                 .baseUrl(OceanTreasuresConstants.BASE_URL)
+                .client(httpClient)
                 .addConverterFactory(GsonConverterFactory.create());
 
         retrofit = builder.build();
