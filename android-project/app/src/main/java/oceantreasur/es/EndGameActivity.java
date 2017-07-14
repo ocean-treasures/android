7fa3b94f0870ab62e331ca1f65faf5502e6cb5c2
Zachary G <zax333@abv.bg>
2017-07-14 15:41:29 +0300

    Implemented fish animation at end game screen

--- android-project/app/src/main/java/oceantreasur/es/EndGameActivity.java
+++ android-project/app/src/main/java/oceantreasur/es/EndGameActivity.java
@@ -1,20 +1,17 @@
 package oceantreasur.es;
 
 import android.content.Intent;
-import android.graphics.Color;
-import android.graphics.Typeface;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
 
-import oceantreasur.es.R;
 import oceantreasur.es.network.OceanTreasuresApplication;
 import oceantreasur.es.view.CustomButton;
-import oceantreasur.es.view.FontManager;
+import oceantreasur.es.view.ViewUtils;
 
 public class EndGameActivity extends BaseActivity {
 
@@ -26,11 +23,8 @@ public class EndGameActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_end_game);
 
-
         this.image = (ImageView) findViewById(R.id.iv_end_game);
         this.button = (CustomButton) findViewById(R.id.btn_play_again);
-
-        //button.setBackgroundColor(Color.TRANSPARENT);
 
         if(image != null) {
             Glide.with(OceanTreasuresApplication.getStaticContext())
@@ -44,6 +38,32 @@ public class EndGameActivity extends BaseActivity {
                    getBackToPlayActivity();
             }
         });
+
+        if (ViewUtils.isTablet(this)) {
+            ImageView logo = (ImageView) findViewById(R.id.iv_logo_end);
+            final ImageView fish1 = (ImageView) findViewById(R.id.fish1);
+            final ImageView fish2 = (ImageView) findViewById(R.id.fish2);
+            final ImageView chest = (ImageView) findViewById(R.id.chest_end);
+
+            View.OnClickListener animate = new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    moveAnimation(fish1, fish2);
+                }
+            };
+
+            logo.setOnClickListener(animate);
+            chest.setOnClickListener(animate);
+        }
+    }
+
+    private void moveAnimation(ImageView fish1, ImageView fish2) {
+        Animation animation = new TranslateAnimation(Animation.ABSOLUTE, 300, Animation.ABSOLUTE, Animation.ABSOLUTE);
+        animation.setDuration(2500);
+        animation.setFillAfter(true);
+
+        fish1.startAnimation(animation);
+        fish2.startAnimation(animation);
     }
 
     private void getBackToPlayActivity() {
