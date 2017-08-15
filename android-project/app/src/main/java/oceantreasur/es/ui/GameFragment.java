34e6a1dfbc57d8ecab2a535f1e7304699361f262
Dean Panayotov <deanpanayotov@gmail.com>
2017-08-15 22:14:16 +0300

    Moved network requests to a separate class and introduced a retry policy (WIP).

--- android-project/app/src/main/java/oceantreasur/es/ui/GameFragment.java
+++ android-project/app/src/main/java/oceantreasur/es/ui/GameFragment.java
@@ -1,7 +1,6 @@
 package oceantreasur.es.ui;
 
 import android.app.Fragment;
-import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.util.Log;
@@ -14,19 +13,12 @@ import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 
-import java.net.SocketTimeoutException;
-
 import oceantreasur.es.MainActivity;
 import oceantreasur.es.R;
-import oceantreasur.OceanTreasuresApplication;
-import oceantreasur.es.network.model.CheckAnswerRequest;
+import oceantreasur.es.network.NetworkManager;
 import oceantreasur.es.network.model.CheckAnswerResponse;
 import oceantreasur.es.network.model.NextWordResponse;
 import oceantreasur.es.network.model.Picture;
-import oceantreasur.es.util.DialogUtil;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
 import static oceantreasur.es.R.id.container;
 
@@ -52,13 +44,28 @@ public class GameFragment extends Fragment {
 
     private android.app.FragmentManager fragmentManagaer;
 
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable
+            Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_game, container, false);
 
         fragmentManagaer = getFragmentManager();
 
         setupFragment(view);
-        getNextWord();
+        NetworkManager.getNextWord(new NetworkManager.SuccessListener<NextWordResponse>() {
+            @Override
+            public void onSuccess(NextWordResponse nextWord) {
+                Fragment fragment = fragmentManagaer.findFragmentById(R.id.container);
+
+                if (fragment instanceof GameFragment) {
+                    GameFragment gameFragment = (GameFragment) fragment;
+
+                    gameFragment.loadImages(nextWord);
+                    gameFragment.setupProgressBar(nextWord.getProgress().getCurrent(), nextWord
+                            .getProgress().getMax());
+                    gameFragment.setTextToTextView(nextWord.getWord().getWord().toString());
+                }
+            }
+        }, getActivity());
 
         return view;
     }
@@ -72,77 +79,6 @@ public class GameFragment extends Fragment {
         }
     }
 
-    public void getNextWord() {
-        Call<NextWordResponse> call = OceanTreasuresApplication.getApi().getNextWord();
-
-        call.enqueue(new Callback<NextWordResponse>() {
-            @Override
-            public void onResponse(Call<NextWordResponse> call, Response<NextWordResponse> response) {
-
-                if (response.code() == 404) {
-
-                    android.app.AlertDialog dialog = DialogUtil.getNeutralImageAlertDialog(R
-                            .string.neutral_button_text, R.drawable.fish, new DialogInterface
-                            .OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialogInterface, int i) {
-                            ((MainActivity) getActivity()).attachFragment(new StartGameFragment());
-                        }
-                    }, getActivity());
-                    dialog.show();
-                } else {
-                    Fragment fragment = fragmentManagaer.findFragmentById(container);
-
-                    if(fragment instanceof GameFragment) {
-                        GameFragment gameFragment = (GameFragment) fragment;
-
-                        nextWord = response.body();
-                        gameFragment.loadImages(nextWord);
-                        gameFragment.setupProgressBar(nextWord.getProgress().getCurrent(), nextWord.getProgress().getMax());
-                        gameFragment.setTextToTextView(nextWord.getWord().getWord().toString());
-                    }
-                }
-            }
-
-            @Override
-            public void onFailure(Call<NextWordResponse> call, Throwable t) {
-                Log.d("ZAX", "ERROR1");
-
-                if(t instanceof SocketTimeoutException){
-                    Log.d("ZAX", "Server Timeout!");
-                    getNextWord();
-                }
-
-            }
-        });
-    }
-
-    public void checkAnswer(int wordId, int picId) {
-        CheckAnswerRequest req = new CheckAnswerRequest(wordId, picId);
-        Call<CheckAnswerResponse> call = OceanTreasuresApplication.getApi().checkAnswer(req);
-
-        call.enqueue(new Callback<CheckAnswerResponse>() {
-            @Override
-            public void onResponse(Call<CheckAnswerResponse> call, Response<CheckAnswerResponse> response) {
-                CheckAnswerResponse serverResponse = response.body();
-
-                Fragment fragment = fragmentManagaer.findFragmentById(container);
-
-                if(fragment instanceof GameFragment) {
-                    GameFragment gameFragment = (GameFragment) fragment;
-
-                    gameFragment.chooseNextFragment(serverResponse);
-                }
-
-                Log.d("ZAX", serverResponse.toString());
-            }
-
-            @Override
-            public void onFailure(Call<CheckAnswerResponse> call, Throwable t) {
-                Log.d("ZAX", "ERROR2");
-            }
-        });
-    }
 
     public void setupProgressBar(int cur, int max) {
         progressBar.setMax(max * STEP_SIZE);
@@ -156,7 +92,22 @@ public class GameFragment extends Fragment {
                 disableImageClicks();
                 Picture pic = (Picture) v.getTag();
                 selectedPictureUrl = pic.getResolvedUrl();
-                checkAnswer(nextWord.getWord().getId(), pic.getId());
+
+                NetworkManager.checkAnswer(nextWord.getWord().getId(), pic.getId(), new
+                        NetworkManager.SuccessListener<CheckAnswerResponse>() {
+                    @Override
+                    public void onSuccess(CheckAnswerResponse response) {
+
+                        Fragment fragment = fragmentManagaer.findFragmentById(container);
+                        if (fragment instanceof GameFragment) {
+                            GameFragment gameFragment = (GameFragment) fragment;
+                            gameFragment.chooseNextFragment(response);
+                        }
+
+                        Log.d("ZAX", response.toString());
+
+                    }
+                }, getActivity());
             }
         };
 
@@ -175,7 +126,8 @@ public class GameFragment extends Fragment {
     public void chooseNextFragment(CheckAnswerResponse response) {
         Bundle data = bundleExtras(response);
 
-        Fragment nextFragmentToDisplay = response.isCorrect() ? new CorrectAnswerFragment() : new WrongAnswerFragment();
+        Fragment nextFragmentToDisplay = response.isCorrect() ? new CorrectAnswerFragment() : new
+                WrongAnswerFragment();
 
         nextFragmentToDisplay.setArguments(data);
         ((MainActivity) getActivity()).attachFragment(nextFragmentToDisplay);
@@ -195,8 +147,8 @@ public class GameFragment extends Fragment {
         word.setText(stringToBeVisualized);
     }
 
-    private void disableImageClicks(){
-        for(ImageView iv : imageViews){
+    private void disableImageClicks() {
+        for (ImageView iv : imageViews) {
             iv.setClickable(false);
             iv.setEnabled(false);
         }
